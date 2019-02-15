4b4291f39093559917cd6c9094787c38e40dd708
chipomwitu <chipomwitu88@gmail.com>
2019-02-15 12:53:29 +0300

    Added viewing nearby shops from a hardcoded location

--- nearbyshops/urls.py
+++ nearbyshops/urls.py
@@ -19,3 +19,9 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+from shops import views
+
+urlpatterns=[
+    path('', views.ShopList.as_view())
+]
--- shops/views.py
+++ shops/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views import generic
+from django.contrib.gis.geos import Point
+from django.contrib.gis.db.models.functions import Distance
+from .models import Shop
 
-# Create your views here.
+# Trafalgar Square
+longitude=-0.123499506
+latitude=51.504831314
+
+user_location=Point(longitude, latitude, srid=4326)
+
+class ShopList(generic.ListView):
+    model=Shop
+    context_object_name='shops'
+    queryset=Shop.objects.annotate(distance=Distance('location', user_location)
+    ).order_by('distance')[0:6]
+    template_name='shops/index.html'
